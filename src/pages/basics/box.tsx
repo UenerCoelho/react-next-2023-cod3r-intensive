149ba52fe36b995ac05cf0f008f08850df15e916
Uener Coelho <uenercoelho2@hotmail.com>
2023-04-28 23:02:49 -0400

    app-initial

---
+++ src/pages/basics/box.tsx
@@ -0,0 +1,11 @@
+import Box from "@/components/basic/Box";
+
+export default function PageBox() {
+  return (
+    <div className="flex gap-7 p-7">
+      <Box>Box 1</Box>
+      <Box>Box 2</Box>
+      <Box>Box 3</Box>
+    </div>
+  )
+}
